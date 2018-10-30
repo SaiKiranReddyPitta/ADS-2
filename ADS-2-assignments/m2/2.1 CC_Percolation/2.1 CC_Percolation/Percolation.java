6b01dc3f068aaf05b69672e7c5e2fc774268b043
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-10-30 23:03:00 +0530

    "ads2_m2_a1_1: 24/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2-assignments/m2/2.1 CC_Percolation/2.1 CC_Percolation/Percolation.java
+++ ADS-2-assignments/m2/2.1 CC_Percolation/2.1 CC_Percolation/Percolation.java
@@ -1,18 +1,60 @@
+/**.
+ * Percolation class.
+ */
 class Percolation {
+    /**.
+     * { var_description }
+     */
+
     private boolean[][] grid;
+    /**.
+     * { var_description }
+     */
+
     private int size;
+    /**.
+     * { var_description }
+     */
+
     private int count = 0;
+    /**.
+     * { var_description }
+     */
+
     private GraphList ghl;
+    /**.
+     * Constructs the object.
+     *
+     * @param      sze   The sze
+     */
+
     Percolation(final int sze) {
         this.size = sze;
         ghl = new GraphList((sze * sze) + 2);
         grid = new boolean[size][size];
     }
+
+    /**.
+     * Gets the index.
+     * time complexity in average case is 1.
+     * @param      i     { parameter_description }
+     * @param      j     { parameter_description }
+     *
+     * @return     The index.
+     */
+
     public int getIndex(final int i, final int j) {
         return (i * size) + j;
     }
-    public void open(final int r, final int c) {
 
+    /**.
+     * { function_description }
+     * time complexity in average case is 1.
+     * @param      r     { parameter_description }
+     * @param      c     { parameter_description }
+     */
+
+    public void open(final int r, final int c) {
             grid[r][c] = true;
             count += 1;
         if (r == 0) {
@@ -34,6 +76,13 @@ class Percolation {
             ghl.addEdge(getIndex(r, c), getIndex(r - 1, c));
         }
     }
+
+    /**.
+     * { function_description }
+     * time complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+
     public boolean percolates() {
         ConnectedComponent concom = new ConnectedComponent(ghl);
         return concom.connected(size * size, (size * size) + 1);
