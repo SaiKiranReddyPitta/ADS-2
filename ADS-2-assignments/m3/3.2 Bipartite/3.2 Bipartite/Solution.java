17a1100df5ac50ae19f4ae528b4b35a3ad6970ff
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-11-05 23:49:39 +0530

    "ads2_m3_a2_1: 5/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m3/3.2 Bipartite/3.2 Bipartite/Solution.java
+++ ADS-2-assignments/m3/3.2 Bipartite/3.2 Bipartite/Solution.java
@@ -5,28 +5,28 @@ import java.util.Scanner;
 /**
  * { item_description }
  */
-final class Solution{
-	/**
-	 * { function_description }
-	 *
-	 * @param      args  The arguments
-	 */
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int vertex = Integer.parseInt(sc.nextLine());
-		Graph graph = new Graph(vertex);
-		int edge = Integer.parseInt(sc.nextLine());
-		for (int i = 0; i < edge; i++) {
-			String[] tokens = sc.nextLine().split(" ");
-			int v = Integer.parseInt(tokens[0]);
-			int w = Integer.parseInt(tokens[1]);
-			graph.addEdge(v, w);
-		}
-		Bipartite bipartite = new Bipartite(graph);
-		if (bipartite.isBipartite()) {
-			System.out.println("Graph is bipartite");
-		} else {
-			System.out.println("Graph is not a bipartite");
-		}
-	}
+final class Solution {
+    /**
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int vertex = Integer.parseInt(sc.nextLine());
+        Graph graph = new Graph(vertex);
+        int edge = Integer.parseInt(sc.nextLine());
+        for (int i = 0; i < edge; i++) {
+            String[] tokens = sc.nextLine().split(" ");
+            int v = Integer.parseInt(tokens[0]);
+            int w = Integer.parseInt(tokens[1]);
+            graph.addEdge(v, w);
+        }
+        Bipartite bipartite = new Bipartite(graph);
+        if (bipartite.isBipartite()) {
+            System.out.println("Graph is bipartite");
+        } else {
+            System.out.println("Graph is not a bipartite");
+        }
+    }
 }
