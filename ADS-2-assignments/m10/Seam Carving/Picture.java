94cd17ba87315c860d95ab374f766a02835db98b
SaiKiranReddyPitta <pittasai01@msitprogram.net>
2018-11-23 20:10:02 +0530

    "ads2_m10_a1_1: 9/9 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2-assignments/m10/Seam Carving/Picture.java
+++ ADS-2-assignments/m10/Seam Carving/Picture.java
@@ -192,7 +192,7 @@ public final class Picture implements ActionListener {
    /**
      * Returns a {@link JLabel} containing this picture, for embedding in a {@link JPanel},
      * {@link JFrame} or other GUI widget.
-     *
+     * Time complexity of O(1).
      * @return the {@code JLabel}
      */
     public JLabel getJLabel() {
@@ -202,6 +202,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Sets the origin to be the upper left pixel. This is the default.
      */
     public void setOriginUpperLeft() {
@@ -209,6 +210,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Sets the origin to be the lower left pixel.
      */
     public void setOriginLowerLeft() {
@@ -216,6 +218,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Displays the picture in a window on the screen.
      */
     public void show() {
@@ -253,7 +256,7 @@ public final class Picture implements ActionListener {
 
    /**
      * Returns the height of the picture.
-     *
+     * Time complexity of O(1).
      * @return the height of the picture (in pixels)
      */
     public int height() {
@@ -262,18 +265,24 @@ public final class Picture implements ActionListener {
 
    /**
      * Returns the width of the picture.
-     *
+     * Time complexity of O(1).
      * @return the width of the picture (in pixels)
      */
     public int width() {
         return width;
     }
-
+    /**
+     * Time complexity of O(1).
+     * @param row [description]
+     */
     private void validateRowIndex(int row) {
         if (row < 0 || row >= height())
             throw new IllegalArgumentException("row index must be between 0 and " + (height() - 1) + ": " + row);
     }
-
+    /**
+     * Time complexity of O(1).
+     * @param col [description]
+     */
     private void validateColumnIndex(int col) {
         if (col < 0 || col >= width())
             throw new IllegalArgumentException("column index must be between 0 and " + (width() - 1) + ": " + col);
@@ -281,7 +290,7 @@ public final class Picture implements ActionListener {
 
    /**
      * Returns the color of pixel ({@code col}, {@code row}) as a {@link java.awt.Color}.
-     *
+     * Time complexity of O(1).
      * @param col the column index
      * @param row the row index
      * @return the color of pixel ({@code col}, {@code row})
@@ -295,6 +304,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Returns the color of pixel ({@code col}, {@code row}) as an {@code int}.
      * Using this method can be more efficient than {@link #get(int, int)} because
      * it does not create a {@code Color} object.
@@ -312,6 +322,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Sets the color of pixel ({@code col}, {@code row}) to given color.
      *
      * @param col the column index
@@ -329,6 +340,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Sets the color of pixel ({@code col}, {@code row}) to given color.
      *
      * @param col the column index
@@ -344,6 +356,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Returns true if this picture is equal to the argument picture.
      *
      * @param other the other picture
@@ -364,6 +377,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(row*col).
      * Returns a string representation of this picture.
      * The result is a <code>width</code>-by-<code>height</code> matrix of pixels,
      * where the color of a pixel is represented using 6 hex digits to encode
@@ -387,6 +401,7 @@ public final class Picture implements ActionListener {
     }
 
     /**
+     * Time complexity of O(1).
      * This operation is not supported because pictures are mutable.
      *
      * @return does not return a value
@@ -397,6 +412,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Saves the picture to a file in either PNG or JPEG format.
      * The filetype extension must be either .png or .jpg.
      *
@@ -410,6 +426,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Saves the picture to a file in a PNG or JPEG image format.
      *
      * @param  file the file
@@ -434,6 +451,7 @@ public final class Picture implements ActionListener {
     }
 
    /**
+    * Time complexity of O(1).
      * Opens a save dialog box when the user selects "Save As" from the menu.
      */
     @Override
--- ADS-2-assignments/m10/Seam Carving/Solution.java
+++ ADS-2-assignments/m10/Seam Carving/Solution.java
@@ -1,154 +1,155 @@
 import java.util.Scanner;
 import java.util.Arrays;
 /**
-* class for solution.
- */
+ * sol class.
+ **/
 final class Solution {
     /**
-     *an empty constructor.
-     */
+     * default constructor.
+     **/
     private Solution() {
 
     }
     /**
-     *the method to print the energies in.
-     * grid format.
-     * time complexity is O(W * H)
-     * W is width and H is height
-     * @param      fileName  The file name
-     */
+     * printEnergies.
+     * @param fileName String.
+     **/
     public static void printEnergies(final String fileName) {
-        Picture picture = new Picture(fileName);
-        StdOut.printf("image is %d pixels wide by %d pixels high.\n",
-         picture.width(), picture.height());
-        SeamCarver sc = new SeamCarver(picture);
-        StdOut.printf("Printing energy calculated for each pixel.\n");
+        Picture pic = new Picture(fileName);
+        System.out.printf("image is %d pixels wide by %d pixels high.\n",
+         pic.width(), pic.height());
+
+        SeamCarver sc = new SeamCarver(pic);
+
+        System.out.printf("Printing energy calculated for each pixel.\n");
+
         for (int row = 0; row < sc.height(); row++) {
             for (int col = 0; col < sc.width(); col++) {
-                StdOut.printf("%9.0f ", sc.energy(col, row));
+                System.out.printf("%9.0f ", sc.energy(col, row));
             }
-            StdOut.println();
+            System.out.println();
         }
     }
     /**
-     *the method is to print the seamline.
-     * time complexity is O(W * H)
-     * W is width and H is height
-     * @param      carver     The carver
-     * @param      seam       The seam
-     * @param      direction  The direction
+     * printSeam method.
+     * @param carver seamCarver.
+     * @param seam int[].
+     * @param direction boolean.
      */
     public static void printSeam(final SeamCarver carver,
      final int[] seam, final boolean direction) {
         double totalSeamEnergy = 0.0;
+        boolean t = true;
+        boolean f = false;
+
         for (int row = 0; row < carver.height(); row++) {
             for (int col = 0; col < carver.width(); col++) {
                 double energy = carver.energy(col, row);
                 String marker = " ";
-                if ((direction && row == seam[col])
-                    || (!direction && col == seam[row])) {
+                if ((direction == t && row == seam[col])) {
+                    marker = "*";
+                    totalSeamEnergy += energy;
+                } else if ((direction == f   && col == seam[row])) {
                     marker = "*";
                     totalSeamEnergy += energy;
                 }
-                StdOut.printf("%7.2f%s ", energy, marker);
+                System.out.printf("%7.2f%s ", energy, marker);
             }
-            StdOut.println();
+            System.out.println();
         }
-        StdOut.printf("Total energy = %f\n", totalSeamEnergy);
-        StdOut.println();
-        StdOut.println();
+        System.out.printf("Total energy = %f\n", totalSeamEnergy);
+        System.out.println();
+        System.out.println();
     }
-    /**
-     *the main method to read the input.
-     *
-     * @param      args  The arguments
-     */
+/**
+ * main method.
+ * @param args args.
+ */
     public static void main(final String[] args) {
         Scanner scan = new Scanner(System.in);
         String cases = scan.nextLine();
-        SeamCarver seamCarver = null;
-        if (cases.length() == 0) {
-            System.out.println("picture is null");
-            return;
-        }
+        SeamCarver seam = null;
         try {
             switch (cases) {
             case "width":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
-                        new Picture("/Files/" + file));
-                    System.out.println(seamCarver.width());
+                    seam = new SeamCarver(new Picture(
+                        "/Files/" + file));
+                    System.out.println(seam.width());
                 }
                 break;
+
             case "height":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
+                    seam = new SeamCarver(
                         new Picture("/Files/" + file));
-                    System.out.println(seamCarver.height());
+                    System.out.println(seam.height());
                 }
                 break;
+
             case "energy":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
                     printEnergies("/Files/" + file);
                 }
                 break;
+
             case "findVerticalSeam":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
-                        new Picture("/Files/" + file));
+                    seam = new SeamCarver(new Picture("/Files/" + file));
                     System.out.println(Arrays.toString(
-                        seamCarver.findVerticalSeam()));
+                        seam.findVerticalSeam()));
                 }
                 break;
-            case "findHorizontalSeam":
+
+              case "findHorizontalSeam":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
-                        new Picture("/Files/" + file));
+                    seam = new SeamCarver(new Picture("/Files/" + file));
                     System.out.println(Arrays.toString(
-                        seamCarver.findHorizontalSeam()));
+                        seam.findHorizontalSeam()));
                 }
                 break;
+
             case "removeVerticalSeam":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
-                        new Picture("/Files/" + file));
-                    int[] verticalSeam = seamCarver.findVerticalSeam();
-                    seamCarver.removeVerticalSeam(verticalSeam);
-                    printSeam(seamCarver, verticalSeam, false);
+                    seam = new SeamCarver(new Picture("/Files/" + file));
+                    int[] verticalSeam = seam.findVerticalSeam();
+                    seam.removeVerticalSeam(verticalSeam);
+                    printSeam(seam, verticalSeam, false);
                 }
                 break;
+
             case "removeHorizontalSeam":
-                while (scan.hasNextLine()) {
+             while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
-                        new Picture("/Files/" + file));
-                    seamCarver.removeHorizontalSeam(
-                        seamCarver.findHorizontalSeam());
-                    int[] horizontalSeam = seamCarver.findHorizontalSeam();
-                    seamCarver.removeHorizontalSeam(horizontalSeam);
-                    printSeam(seamCarver, horizontalSeam, true);
+                    seam = new SeamCarver(new Picture("/Files/" + file));
+                    seam.removeHorizontalSeam(
+                        seam.findHorizontalSeam());
+                    int[] horizontalSeam = seam.findHorizontalSeam();
+                    seam.removeHorizontalSeam(horizontalSeam);
+                    printSeam(seam, horizontalSeam, true);
                 }
                 break;
+
             case "removeHorizontalSeam removeVerticalSeam":
                 while (scan.hasNextLine()) {
                     String file = scan.nextLine();
-                    seamCarver = new SeamCarver(
-                        new Picture("/Files/" + file));
-                    int[] horizontalSeam = seamCarver.findHorizontalSeam();
-                    seamCarver.removeHorizontalSeam(horizontalSeam);
-                    int[] verticalSeam = seamCarver.findVerticalSeam();
-                    seamCarver.removeVerticalSeam(verticalSeam);
+                    seam = new SeamCarver(new Picture("/Files/" + file));
+                    int[] horizontalSeam = seam.findHorizontalSeam();
+                    seam.removeHorizontalSeam(horizontalSeam);
+                    int[] verticalSeam = seam.findVerticalSeam();
+                    seam.removeVerticalSeam(verticalSeam);
                     printEnergies("/Files/" + file);
                 }
                 break;
+
             default:
-                seamCarver = new SeamCarver(null);
+                seam = new SeamCarver(null);
                 break;
             }
         } catch (Exception ex) {
